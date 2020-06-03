c89a367654934db6a1fdc1eee1a9ae29cd0dbd09
dianestephani <diane.stephani@gmail.com>
2020-06-03 14:24:57 -0700

    we've done some things

--- js/app.js
+++ js/app.js
@@ -44,7 +44,7 @@ const dropDown = () => {
   })
 }
 
-$('select').on('change', function(){
+$('select').on('change', function () {
   let $selection = $(this).val();
   console.log($selection)
   $('section').hide()
@@ -61,28 +61,50 @@ $.ajax('data/page-1.json', { method: 'GET', dataType: 'JSON' })
     dropDown();
   })
 
-  $.ajax('data/page-1.json', { method: 'GET', dataType: 'JSON' })
-  .then(photos => {
-    photos.forEach(value => {
-      new Photo(value).render();
-    })
-  })
+// Both ajax calls inside if/else statement, reference value of the button that was pushed to decide which ajax gets called.**
+// Single ajax call: make first ajax file path a template literal, change page 1 to just "variable" that depends on which button was clicked.
 
-$('button').on('submit', function(){
+// $.ajax('data/page-1.json', { method: 'GET', dataType: 'JSON' })
+
+let runSecondHalf = () => {
+  // render half of the array
+  // direct it to the second page
+  // will end up calling...
+
+  // let newArray = [];
+  // let secondArray = allPhotos.slice(20, 39);
+  // newArray.push(secondArray);
+  // return newArray;
+  // let.html = Mustache.render(newArray)
+  $.ajax('data/page-2.json', { method: 'GET', dataType: 'JSON' })
+    .then(photos => {
+      photos.forEach(value => {
+        new Photo(value).render();
+      })
+    })
+}
+
+$('button').on('click', function () {
   console.log(event);
+  // console.log(allPhotos.slice(20, 39));
+  allPhotos = [];
+  // $('img').attr('');
+  // $('h2').text('');
+  // $('p').text('');
+  runSecondHalf();
 })
 
-  $.ajax('data/page-2.json', { method: 'GET', dataType: 'JSON' })
+
+// add click event
 
 
+// Add navigation button xxx
+// CLear page, render second set of data
+// Render correct data to the right page
+// Reset the filters (sean: filters don't change, stay basically the same)
+// Repopulate them using only keywords from the images currently being displayed.
 
-  // Add navigation button xxx
-  // CLear page, render second set of data
-  // Render correct data to the right page
-  // Reset the filters (sean: filters don't change, stay basically the same)
-  // Repopulate them using only keywords from the images currently being displayed.
-
-  // Handle the event, render all the new data when that happens
-  // BUtton = submit event. .on submit, etc.
-  // COuld put entire ajax into an event handler
-  // COuld create a new value and run a new loop
+// Handle the event, render all the new data when that happens
+// BUtton = submit event. .on submit, etc.
+// COuld put entire ajax into an event handler
+// COuld create a new value and run a new loop
